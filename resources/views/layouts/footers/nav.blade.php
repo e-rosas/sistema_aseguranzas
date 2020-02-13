a2e99747641c9187409f358b8a5348e1bf84601b
Edgar Rosas <gary.blurk@gmail.com>
2020-02-12 16:14:06 -0800

    Dashboard base

---
+++ resources/views/layouts/footers/nav.blade.php
@@ -0,0 +1,10 @@
+<div class="row align-items-center justify-content-xl-between">
+    <div class="col-xl-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; {{ now()->year }} <a href="http://www.hospitalmexico.org" class="font-weight-bold ml-1" target="_blank">Hospital Mexico</a>
+        </div>
+    </div>
+    <div class="col-xl-6">
+        
+    </div>
+</div>
